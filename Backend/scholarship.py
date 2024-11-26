0eefa63e108f7ca54ee2ba4daecccfbc00e4dedb
Abdul Keita <akk6047@psu.edu>
2024-11-26 00:43:43 -0500

    testing out new branch

--- Backend/scholarship.py
+++ Backend/scholarship.py
@@ -5,6 +5,7 @@ from google.oauth2.service_account import Credentials # for google cloud authent
 from datetime import datetime
 import re # for regex for dates, as of rn only for JVL
 
+
 class NSBEScholarshipsJVL:
     def __init__(self, urls):
         self.urls = urls
