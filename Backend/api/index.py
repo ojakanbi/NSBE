557450cbfe36eb44fef2872cc360d4670beb60b1
Oyindamola Joshua Akanbi <ojakanbi@oyindamolas-mbp.mynetworksettings.com>
2024-11-26 20:58:06 -0500

    CORS

--- Backend/api/index.py
+++ Backend/api/index.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"]) # Allow requests from the frontend
+CORS(app, origins=["https://frontend-nsbe-1.vercel.app"])
 
 
 urls = [
